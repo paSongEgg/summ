006775647b18ca65011d5e145087bd7585d66392
Monu <kdie44@gmail.com>
2022-01-10 17:01:06 +0900

    충돌 해결

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import AppRouter from './Router.js';
-import Navigation from './components/Navigation.js';
 
 function App() {
     return (
         <div className="App">
-            <Navigation/>
             <AppRouter />
         </div>
     )
