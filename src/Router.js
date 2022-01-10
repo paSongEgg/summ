3d86febc8314c63fb3ae71afd81e697a1ab7f927
Monu <49127827+KimDahui42@users.noreply.github.com>
2022-01-10 17:29:54 +0900

    minor change
    네비게이션 보이게 수정

--- src/Router.js
+++ src/Router.js
@@ -2,10 +2,12 @@ import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Home from './routes/Home.js';
 import Auth from './routes/Auth.js';
 import Profile from './routes/Profile.js';
+import Navigation from './components/Navigation.js';
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
+            <Navigation/>
             <Routes>
                 <Route exact path='/' element={<Home />} />
                 <Route path='/auth' element={<Auth />} />
