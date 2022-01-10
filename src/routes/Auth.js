0764d8b9b34636fc692bf3f6e32ca00d666bf105
Monu <kdie44@gmail.com>
2022-01-10 17:17:34 +0900

    로그인 페이지 생성

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,6 +1,53 @@
+import { useState } from "react";
+
 const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccout] = useState(true);
+
+    const onChange = (event) => {
+        const {
+            target: { name, value },
+        } = event;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+    const loginToCreate = () => setNewAccout((prev) => !prev);
     return (
-        <div/>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    required
+                    value={email}
+                    onChange={onChange}
+                />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={password}
+                    onChange={onChange}
+                />
+                <input
+                    type="submit"
+                    value={newAccount ? "계정생성" : "로그인"}
+                />
+            </form>
+            <span onClick={loginToCreate}>
+                {newAccount ? "로그인" : "계정생성"}
+            </span>
+        </div>
     );
 };
 export default Auth;
