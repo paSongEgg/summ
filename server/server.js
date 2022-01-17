398d08df0272cbbcd1da95fc217d8057b5d26e6d
Soyeong Yun <kkee2512@naver.com>
2022-01-17 19:52:20 +0900

    .

--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ const db = require('./config/db');
 
 app.get('/', (req, res) => {
   db.connect(function (err) {
-    db.query("SELECTED * FROM user_table", (err, data) => {
-      if (!err) res.send({ data: data });
-      else res.send(err);
+      db.query("SELECTED * FROM user_table", (err, data) => {
+          if (!err) res.send({ data: data });
+          else res.send(err);
+      })
     });
   });
 });
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
+import styles from "styles/Quiz.module.css";
 import axios from "axios";
 import {
   Container,
