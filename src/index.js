edf94ba9c3a477fa97019c8b5858a344d18cf0e4
sychaee <sychae1216@gmail.com>
2022-01-22 04:01:53 +0900

    login with aws

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App.js';
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
 
 ReactDOM.render(
     <React.StrictMode>
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -2,10 +2,13 @@ import { useState } from "react";
 
 import { Auth } from 'aws-amplify'
 
+import { Container, Col, Button, Form } from "react-bootstrap"
+
+
 const Authorization = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [newAccount, setNewAccout] = useState(true);
+    const [newAccount, setNewAccount] = useState(true);
 
     const onChange = (event) => {
         const {
@@ -20,40 +23,56 @@ const Authorization = () => {
 
     const onSubmit = (event) => {
         event.preventDefault();
+        let data;
+        if (newAccount) {
+            //await createUser
+        } else {
+            //await signIn
+        }
     };
-    const loginToCreate = () => setNewAccout((prev) => !prev);
+    const loginToCreate = () => setNewAccount((prev) => !prev);
     return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input
-                    name="email"
-                    type="email"
-                    placeholder="Email"
-                    required
-                    value={email}
-                    onChange={onChange}
-                />
-                <input
+        <Container fluid>
+                <Col sm={4}>
+                <form onSubmit={onSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control 
+                        name="email"
+                        type="email"
+                        placeholder="Email"
+                        required
+                        value={email}
+                        onChange={onChange} />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control 
                     name="password"
                     type="password"
                     placeholder="Password"
                     required
                     value={password}
-                    onChange={onChange}
-                />
-                <input
+                    onChange={onChange} />
+                </Form.Group>
+                <Button 
+                    variant="primary" 
                     type="submit"
-                    value={newAccount ? "계정생성" : "로그인"}
-                />
+                    value={newAccount ? "계정생성" : "로그인"}>
+                    Submit
+                </Button>
             </form>
             <span onClick={loginToCreate}>
                 {newAccount ? "로그인" : "계정생성"}
-            </span>
-            <button
-            onClick={() => Auth.federatedSignIn({ provider: "Google" })}>
-                <img src="img/btn_google_signin_light_normal_web.png" style={{height:"45px", width:"190px"}}/>
-            </button>
-        </div>
+            </span>{' '}
+            <Button
+                onClick={() => Auth.federatedSignIn({ provider: "Google" })}
+                variant="outline-danger">
+                    Sign in with Google
+            </Button>
+            </Col>
+        </Container>
     );
 };
 export default Authorization;
