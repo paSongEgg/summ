98013e810c506d9262b6c4bbf27e7b91ced45891
sychaee <sychae1216@gmail.com>
2022-02-14 21:43:57 +0900

    Home 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,24 +4,17 @@ import { useState } from "react";
 import styles from "../styles/Home.module.css"
 
 const Home=()=>{
-    const [isToday,setIsToday]=useState(true);
-    const onOptionClick=()=>setIsToday((prev)=>!prev);
     return(
         <section className={styles.home_container}>
-            <button className={styles.home_button_changeOption} onClick={onOptionClick}>통합</button>            
-            {isToday?(
-                <TodayNews/>
-            ):(
-                <div className={styles.theme}>
-                    <button className={styles.button_theme}>정치</button>
-                    <button className={styles.button_theme}>경제</button>
-                    <button className={styles.button_theme}>사회</button>
-                    <button className={styles.button_theme}>생활/문화</button>
-                    <button className={styles.button_theme}>IT/과학</button>
-                    <button className={styles.button_theme}>세계</button>
-                    <Themed/>
-                </div>
-            )}
+            <div className={styles.theme}>
+                <button className={styles.home_button_changeOption}>통합</button>            
+                <button className={styles.button_theme}>정치</button>
+                <button className={styles.button_theme}>경제</button>
+                <button className={styles.button_theme}>사회</button>
+                <button className={styles.button_theme}>생활/문화</button>
+                <button className={styles.button_theme}>IT/과학</button>
+                <button className={styles.button_theme}>세계</button>
+            </div>
         </section>
     )
 }
