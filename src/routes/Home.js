4882a1d840ce038f351ffdee811d5b4cd5ed4590
Monu <kdie44@gmail.com>
2022-02-21 13:07:39 +0900

    초기 함수 코드 작성 완료/nav 바 클릭시 리액트 hook오류 발생

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,15 @@
 import Themed from "components/Themed";
 import TodayNews from "components/TodayNews";
 import { useState } from "react";
-import styles from "../styles/Home.module.css"
+import styles from "styles/Home.module.css"
 
 const Home=()=>{
     const themes=["정치","경제","사회","생활/문화","IT/과학","세계"];
     const [isThemeClicked,setIsThemeClicked]=useState(false);
     const [clickedTheme,setClickedTheme]=useState("");
 
-    const onThemeClick=(clicked_id)=>{
-        setClickedTheme(clicked_id);
+    const onThemeClick=(e)=>{
+        setClickedTheme(e.target.id);
         setIsThemeClicked(true);
     }
 
@@ -22,7 +22,7 @@ const Home=()=>{
             <div className={styles.theme}>
                 <button onClick={onAllClick} className={styles.home_button_changeOption} id="통합">통합</button>       
                 {themes.map((theme)=>(
-                    <button onClick={onThemeClick(this.id)} id={theme} className={styles.button_theme}>{theme}</button>
+                    <button onClick={onThemeClick} key={theme} id={theme} className={styles.button_theme}>{theme}</button>
                 ))}
             </div>
             {(isThemeClicked)?
