8d58b565091b0cc0f0cd8ede983dde03e37b02a5
Soyeong Yun <kkee2512@naver.com>
2022-02-24 18:01:41 +0900

    크롤러 수정

--- src/crawler/NewsCrawler.py
+++ src/crawler/NewsCrawler.py
@@ -8,16 +8,39 @@ from sklearn.preprocessing import normalize
 import numpy as np
 
 
-def get_href(soup) :
+def get_href(soup, option) :
     result = []
-    div = soup.find("div", class_="list_body newsflash_body")
 
-    for dt in div.find_all("dt", class_="photo") :
-        result.append(dt.find("a")["href"])
+    if "날짜" in option : 
+        div = soup.find('div', { 'class' : 'list_body newsflash_body'})
+
+        for dt in div.find_all('dt', {'class' : 'photo'}) :
+            result.append(dt.find('a')['href'])
+
+    elif "키워드" in option :
+        try :
+            ul_list = soup.find('ul', {'class' : 'list_news'})
+            li_list = ul_list.find_all('li', {'id' : re.compile('sp_nws.*')})
+            area_list = [li.find('div', {'class' : 'news_area'}) for li in li_list]
+            info_list = [area.find('div', {'class' : 'news_info'}) for area in area_list]
+            group_list = [info.find('div', {'class' : 'info_group'}) for info in info_list]
+            a_list = []
+            
+            for group in group_list :
+                a = group.find('a', {'class' : 'info'})
+                a2 = a.next_sibling.next_sibling
+                a_list.append(a2)
+
+            for al in a_list :
+                al = al.get('href')
+                result.append(al)
+
+        except AttributeError as e :
+            print("e")
 
     return result
 
-def get_request(date, section) :
+def get_request_ds(date, section) :
     headers = {
         'referer' : 'https://www.naver.com/',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
@@ -30,15 +53,43 @@ def get_request(date, section) :
     
     return req
 
-def crawling_news(soup) :
-    #제목
-    title = soup.find("div", "article_info").find("h3").string
-    title = re.sub('\t|\r|\n| ', '', title)
-    #본문
-    content = soup.find("div","_article_body_contents").text
-    content = re.sub('\xa0|\t|\r|\n|', '', content)
+def get_request_sp(search, period) :
     
-    return title + content
+        headers = {
+        'referer' : 'https://www.naver.com/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+        }
+
+        pds = { "1" : 7, "2" : 8, "3" : 9, "4" : 10, "5" : 11, "6" : 12}
+
+        url = "https://search.naver.com/search.naver?"
+        req = requests.get(url, headers = headers, params = { "where" : "news", "sm" : "tab_opt", "sort" : "0", "query" : search, "pd" : pds[period]})
+
+        return req
+
+def crawling_news(soup) :
+    dic = {}
+    #제목
+    try : 
+        title = soup.find('div', {'class' : 'article_info'}).find('h3').string
+    except AttributeError :
+        title = soup.find('div', {'class' : 'end_ct_area'}).find('h2').string
+    except Exception as e :
+        print("에러 발생")
+
+    title = re.sub('\t|\r|\n| ', '', title)
+    
+    #본문
+    try :
+        content = soup.find('div',{'id' : 'articleBodyContents'}).text
+    except AttributeError :
+        content = soup.find('div',{'id' : 'articeBody'}).text
+    except Exception as e :
+        print("에러 발생")
+        
+    content = re.sub('\xa0|\t|\r|\n|', '', content)
+
+    return {'title' : title, 'content' : content}
 
 
 class SentenceTokenizer(object) :
@@ -47,8 +98,12 @@ class SentenceTokenizer(object) :
         self.kkma = Kkma()
         self.okt = Okt()
         #불용어
-        self.stopwords = ["연합뉴스", "데일리", "동아일보", "중앙일보", "조선일보", "YTN", "News1", "기자", "따라", "의해", "을", "를", "에", "의", "가"]
-
+        self.stop_words_list = ["머니투데이", "이데일리" , "연합뉴스", "데일리", "동아일보", "중앙일보", "조선일보", "YTN", "News1", "기자", "특파원", "아", "어",
+             "나", "우리", "저희", "따라", "의해", "을", "를", "에", "의", "가", "이", "있", "하", "것", "들", "그", "되", "수", "보", "않", "없", "사람", "주", "아니", "등", 
+             "같", "때", "년", "한", "지", "대하", "오", "말", "일", "그렇", "위해", "때문", "그것", "두", "말하", "알", "그러나", "받", "못하", "일", "그런", "또", "문제", "더",
+             "많", "그리고", "좋", "크", "따르", "중", "나오", "가지", "씨", "시키", "만들", "지금", "생각하", "그러", "속", "하나", "집", "살", "모르", "적", "월", "데", "자신",
+             "안", "어떤", "내", "경우", "생각", "시간", "그녀", "다시", "이런", "앞", "보이", "번", "다른", "어떻", "개", "전", "들", "사실", "이렇", "점", "싶", "말", "정도",
+             "좀", "원", "잘", "통하", "소리", "놓", "비추어", "형식으로", "의해서", "만큼", "즉", "인하여", "까닭으로", "오늘", "내일", "모레", "뉴스", "지난"]
     
     def text2sentences(self, text) :
         #문장 분리하여 리스트 생성
@@ -67,14 +122,14 @@ class SentenceTokenizer(object) :
         for s in sentences :
             if s != '':
                 nouns.append(' '.join([noun for noun in self.okt.nouns(str(s))
-                                      if noun not in self.stopwords and len(noun) > 1]))
+                                      if noun not in self.stop_words_list and len(noun) > 1]))
                 
         return nouns
 
 
 class GraphMatrix(object) :
     def __init__(self) :
-        self.cnt_vec = CountVectorizer()
+        self.cnt_vec = CountVectorizer() #단어 출현 빈도로 문서 벡터화
     
     def build_words_graph(self, sentence) :
         #명사로 이루어진 문장 입력 받고 matrix 생성 후 word graph와 {idx : word} 형태의 dic 리턴
@@ -85,12 +140,11 @@ class GraphMatrix(object) :
 
 
 class Rank(object):
-    #Text Rank
     def get_ranks(self, graph, d=0.85) : #d = damping factor
         A = graph
         matrix_size = A.shape[0]
         for id in range(matrix_size):
-            A[id, id] = 0
+            A[id, id] = 0 ##대각행렬 원소 1을 0으로 치환
             link_sum = np.sum(A[:,id])
             if link_sum != 0:
                 A[:, id] /= link_sum
@@ -98,7 +152,7 @@ class Rank(object):
             A[id, id] = 1
 
         B = (1-d) * np.ones((matrix_size, 1))
-        ranks = np.linalg.solve(A, B)
+        ranks = np.linalg.solve(A, B) #연립방정식 Ax = b
         
         return {idx: r[0] for idx, r in enumerate(ranks)}
 
@@ -138,45 +192,66 @@ class TextRank(object) :
         return keywords
 
 
-def main() :
+def main(date, section) :
     headers = {
         'referer' : 'https://www.naver.com/',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
         }
         
     href_list = []
-    news_content = []
+    news_content = {}
+    i = 0
 
-    date = input("날짜 검색 (ex.YYYYMMDD) : ")
-    section = input("검색할 섹션 선택 (정치 / 경제 / 사회 / 생활문화 / 세계 / IT과학) : ")
-    req = get_request(date, section)
-    soup = BeautifulSoup(req.text, "html.parser")
+    option = input("날짜 검색 or 키워드 검색 : ")
 
-    href_list = get_href(soup)
+    if "날짜" in option :
+        date = input("날짜 검색 (ex.YYYYMMDD) : ")
+        section = input("검색할 섹션 선택 (정치 / 경제 / 사회 / 생활문화 / 세계 / IT과학) : ")
+        req = get_request_ds(date, section)
+        soup = BeautifulSoup(req.text, "html.parser")
 
-    for href in href_list :
-        href_req = requests.get(href, headers = headers)
-        href_soup = BeautifulSoup(href_req.text, "html.parser")
-        news_content.append(crawling_news(href_soup))
+        href_list = get_href(soup, option)
 
-    print()
+        for href in href_list :
+            if(href is None) :
+                continue
+            href_req = requests.get(href, headers = headers)
+            href_soup = BeautifulSoup(href_req.text, "html.parser")
+            news_content[i] = {'title' : crawling_news(href_soup).get('title'), 'content' : crawling_news(href_soup).get('content')}
+
+        
+    elif "키워드" in option :
+        search = input("검색할 키워드 : ")
+        period= input("최근 몇 시간 동안의 뉴스를 검색하시겠습니까? (1~6) : ")
+
+        req = get_request_sp(search, period)
+        soup = BeautifulSoup(req.text, 'html.parser')
+
+        href_list = get_href(soup, option)
+
+        for href in href_list :
+            if(href is None) :
+                continue
+            href_req = requests.get(href, headers = headers)
+            href_soup = BeautifulSoup(href_req.text, 'html.parser')
+            news_content[i] = {'title' : crawling_news(href_soup).get('title'), 'content' : crawling_news(href_soup).get('content')}
+            
     print(len(news_content), "개의 기사가 검색됨.")
     print()
 
-    for i in range(len(news_content)) :
-    #본문 가져오기
-
-        print("뉴스 기사 링크 >\n")
-        print(href_list[i])
+    for idx in range(0, len(href_list)) :
+        print("링크 : ")
+        print(href_list[idx])
         print()
-        print("뉴스 기사 본문 >\n")
-        print(news_content[i])
+        print("제목 : ")
+        print(news_content[idx]['title'])
+        print("본문 : ")
+        print(news_content[idx]['content'])
         print()
 
-        textrank = TextRank(news_content[i])
-        print(" 키워드 > ", textrank.keywords())
+        textrank = TextRank(news_content[idx]['title'] + " " + news_content[idx]['content'])
+        print(" 키워드 : ", textrank.keywords())
         print("\n\n\n")
-
         
 
 if __name__ == "__main__" :
